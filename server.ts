4add98562c42711be33ff5fc1cc97ee1b5562a9a
kaushik <kaushik@dream11.com>
2021-04-12 18:34:34 +0530

    uwebsocket

--- server.ts
+++ server.ts
@@ -1,19 +1,39 @@
-import { ApolloServer } from "apollo-server-express";
-import express = require("express");
+const uws = require("uWebSockets.js");
+const gql = require("graphql");
 import { Schema } from "./schema-builder";
 
-const app = express();
+const { graphql } = gql;
 
-const apolloServer = new ApolloServer({
-  schema: Schema,
-  context: (expressContext) => {
-    return expressContext;
-  },
-  playground: true,
-});
-apolloServer.applyMiddleware({ app });
-app.get("/health", (request: any, response: any) => {
-  return response.json({ status: "Success" });
-});
+uws
+  .App()
+  .post("/graphql", async (res: any, req: any) => {
+    res.onAborted(() => {
+      throw new Error("Aborted");
+    });
+    const contentType = req.getHeader("content-type");
+    const body = await readBody(res);
+    let query, variables;
+    if (contentType === "application/json") {
+      try {
+        ({ query, variables } = JSON.parse(body as string));
+      } catch (e) {
+        console.error("JSON.parse", e);
+        res.writeStatus("400");
+        return res.end(JSON.stringify({ name: e.name, message: e.message }));
+      }
+    }
+    const response = await graphql(Schema, query, {}, {}, variables);
+    res.end(JSON.stringify(response));
+  })
+  .listen(4001, () => {});
 
-app.listen(1452);
+const readBody = (res: any) =>
+  new Promise((resolve) => {
+    let buffer: any;
+    res.onData((ab: any, isLast: any) => {
+      const chunk = Buffer.from(ab);
+      buffer = buffer ? Buffer.concat([buffer, chunk]) : chunk;
+      if (!isLast) return;
+      resolve(buffer.toString());
+    });
+  });
